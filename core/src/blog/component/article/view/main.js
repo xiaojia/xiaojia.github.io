6f62322f79753d0d4b5a4b461a2042d0f44b00df
menmaojia <menmaojia@mucang.cn>
2016-06-03 16:35:49 +0800

    项目初始化

--- core/src/blog/component/article/view/main.js
+++ core/src/blog/component/article/view/main.js
@@ -14,7 +14,7 @@ Simple.Module({
 
         id: module.id,
 
-        template: function(i,a,e,t){"use strict";var c=this,l=(c.$helpers,i.props),n=c.$each,s=(i.$value,i.$index,function(){var i="".concat.apply("",arguments);return 1==this?i:r+=i}),d=c.$escape,r="";return r+='<div class="article">',l?n(l,function(i,a){r+='<div class="item"><div class="title"><a href="">',s.call(!1,d(i.title)),r+="</a></div></div>"}):r+='<div class="loading">loading...</div>',r+="</div>",new String(r)},
+        template: function(i,a,s,n){"use strict";var e=this,t=(e.$helpers,i.props),c=e.$each,l=(i.$value,i.$index,function(){var i="".concat.apply("",arguments);return 1==this?i:v+=i}),d=e.$escape,v="";return v+='<div class="article">',t?c(t,function(i,a){v+='<div class="item"><div class="title"><a href="">',l.call(!1,d(i.title)),v+='</a><span class="time">2016/10/10</span><span class="name">\u674e\u8fc5</span></div><div class="content">\u5185\u5bb9</div></div>'}):v+='<div class="loading">loading...</div>',v+="</div>",new String(v)},
 
         style: ["./main"]
 

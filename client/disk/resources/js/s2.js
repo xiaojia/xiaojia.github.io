4442dd4ed1463a57221d49411022d8573c9e8bfe
小佳 <xiaojia@xiaojia.local>
2019-05-10 18:49:59 +0800

    更新

--- client/disk/resources/js/s2.js
+++
@@ -1,1 +0,0 @@
-window.Simple={config:{name:"blog",container:document.body,root:location.pathname,packages:{blog:"core/src/blog/",common:"core/src/common/",article:"article/",simple:"simple-framework/simple/src/"}}};
